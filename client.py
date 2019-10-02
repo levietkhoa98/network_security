e43dcc3588a7529c1845505096be6c27d92cd098
DuongSonn <41414909+DuongSonn@users.noreply.github.com>
2019-10-02 19:44:08 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -8,9 +8,15 @@ import threading
 from base64 import b64encode
 import base64
 import json
+import time
+from tkinter import *
+import queue
+from functools import partial
 
-def sendeMsg(key,client):
-    sendMsg = input("Enter your message").encode()
+msgQueue = queue.Queue()
+
+def sendeMsg(key,client,message):
+    sendMsg = message.encode()
     # print(sendMsg)
     key = key[:16].encode()
     # print(key)
@@ -23,8 +29,151 @@ def sendeMsg(key,client):
     # print(eMsg)
     client.send(eMsg)
 
+def recvdMsg(key,client,msgQueue):
+    eMsg = client.recv(1024).decode()
+    b64 = json.loads(eMsg)
+    nonce = base64.b64decode(b64['nonce'])
+    ct = base64.b64decode(b64['ciphertext'])
+    # print(eMsg)
+    key = key[:16].encode()
+    # print(key)
+    aesDecrypt = AES.new(key,AES.MODE_CTR,nonce=nonce)
+    # print(aesDecrypt)
+    dMsg = aesDecrypt.decrypt(ct).decode()
+    print("New mess from client ",client ," : " , dMsg)
+    if (dMsg == "Create new user successfully" or dMsg == "Login successfully" or (dMsg.startswith('[') and dMsg.endswith(']'))):
+        msgQueue.put(dMsg)
+
+def Signup(key,client):
+	global pwordE
+	global nameE
+
+	global roots
+
+	roots = Tk()
+	roots.title('Signup')
+	instruction = Label(roots, text='register')
+	instruction.grid(row=0, column=0, sticky=E)
+
+	nameL = Label(roots, text='New Username: ')
+	pwordL = Label(roots,text='New Password: ')
+	nameL.grid(row=1, column=0, sticky=W)
+	pwordL.grid(row=2, column=0, sticky=W)
+
+	nameE = Entry(roots)
+	pwordE = Entry(roots, show='*')
+	nameE.grid(row=1, column=1)
+	pwordE.grid(row=2, column=1)
+
+	signupButton = Button(roots, text='Signup', command=partial(FSSignup,key,client))
+	signupButton.grid(columnspan=2, sticky=W)
+	roots.mainloop()
+
+def FSSignup(key,client):
+    print(msgQueue.empty())
+    sendMsg = "signup " + nameE.get() + " " + pwordE.get()
+    threadSend = threading.Thread(target=sendeMsg,args=(key,client,sendMsg,))
+    threadSend.start()
+    threadSend.join()
+
+    threadRecv = threading.Thread(target=recvdMsg,args=(key,client,msgQueue,))
+    threadRecv.start()
+    threadRecv.join()
+
+    if ( msgQueue.get() == "Create new user successfully" ):
+        roots.destroy()
+        print(msgQueue.empty())
+        Login(key,client)
+
+def Login(key,client):
+	global nameEL
+	global pwordEL
+	global rootA
+
+	rootA = Tk()
+	rootA.title('Login')
+
+	instruction = Label(rootA, text='Login\n')
+	instruction.grid(sticky=E)
+
+	nameL = Label(rootA, text='Username: ')
+	pwordL = Label(rootA, text='Password: ')
+	nameL.grid(row=1, sticky=W)
+	pwordL.grid(row=2, sticky=W)
+
+	nameEL = Entry(rootA)
+	pwordEL = Entry(rootA, show='*')
+	nameEL.grid(row=1, column=1)
+	pwordEL.grid(row=2, column=1)
+
+	loginB = Button(rootA, text='submit', command=partial(CheckLogin,key,client))
+	loginB.grid(columnspan=2, sticky=W)
+
+	rmuser = Button(rootA, text='register', fg='red', command=partial(DelUser,key,client))
+	rmuser.grid(columnspan=2, sticky=W)
+	rootA.mainloop()
+
+def CheckLogin(key,client):
+    print(msgQueue.empty())
+    sendMsg = "login " + nameEL.get() + " " + pwordEL.get()
+    threadSend = threading.Thread(target=sendeMsg,args=(key,client,sendMsg,))
+    threadSend.start()
+    threadSend.join()
+
+    threadRecv = threading.Thread(target=recvdMsg,args=(key,client,msgQueue,))
+    threadRecv.start()
+    threadRecv.join()
+    if ( msgQueue.get() == "Login successfully" ):
+        threadRecv = threading.Thread(target=recvdMsg,args=(key,client,msgQueue,))
+        threadRecv.start()
+        threadRecv.join()
+
+        print(msgQueue.empty())
+        rootA.destroy()
+        data = msgQueue.get()
+        chat(key,client,data)
+    else:
+        r = Tk()	
+        r.title('D:')
+        r.geometry('150x50')
+        rlbl = Label(r, text='\n[! Invalid Login')
+        rlbl.pack()
+        r.mainloop()	
+
+def DelUser(key,client):
+	rootA.destroy()
+	Signup(key,client)
+
+def chat(key,client,data):
+	global rootsC
+
+	rootsC = Tk()
+	rootsC.title('chat')
+	instruction = Label(rootsC, text='chat' )
+	instruction.grid(row=0, column=0, sticky=E)
+	for i,name in enumerate(data):
+		x=i+2
+		btn_row = Button(rootsC, text=name)
+		btn_row.grid(row=x)
+
+	
+	
+	userName=Label(rootsC,text='hoang',width = 20)
+
+	userName.grid(row =5 , column=0)
+
+	chatBox=Label(rootsC,width = 40 , height = 20 , bd =1 , relief='solid')
+	chatBox.grid(row=6 , column =0)
+	chatF=Entry(rootsC , width = 33)
+	chatF.grid(columnspan=2 ,row=7, column=0, sticky=W )
+	addButton1 = Button(rootsC, text='send', command='', width=10)
+	addButton1.grid(columnspan=2, row=7, column=0, sticky=E)
+
+	rootsC.mainloop()
+
 def main():
     serverAddress = "127.0.0.1"
+    # serverAddress = "192.168.1.1"
     serverPort = 1600
 
     #Tạo public key và private key    
@@ -54,8 +203,9 @@ def main():
     en_digest = en_object.hexdigest()
     print(en_digest)
     if (en_digest):
-        while True:
-            threading.Thread(target=sendeMsg,args=(en_digest,client,)).start()
+        Login(en_digest,client)
+        while True:            
+            print(threading.active_count())
         client.close()
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -8,19 +8,26 @@ import hashlib
 import os
 import base64
 import json
+import time
 
 clientConnected_Socket = []
 clientConnected_Address = []
 clientConnected_SessionKey = []
 clientConnected_SignedIn = []
+threadLock = False
+
+mydb = mysql.connector.connect(host='localhost',database='anm',user='anm',password='123abnkakashi',port='1998')
+mycursor = mydb.cursor()
+
 welcomeMsg = "Welcome new client please sign in or sign up"
+
 def main():
     #kkết nối csdl
-    mydb = mysql.connector.connect(host='localhost',database='anm',user='anm',password='123abnkakashi',port='1998')
     if mydb.is_connected():
         print("Connected to DataBase")
 
     serverAddress = "127.0.0.1"
+    # serverAddress = "192.168.1.1"
     serverPort = 1600
 
     # khởi tạo socket
@@ -32,7 +39,12 @@ def main():
     while True:
         clientSock, clientAddress = server.accept()
         #tạo thread khi có client mới
-        threading.Thread(target=clientthread,args=(clientSock,clientAddress,)).start()
+        threadConn = threading.Thread(target=clientthread,args=(clientSock,clientAddress,))
+        threadConn.start()
+        if (threadLock == True):
+            threadConn.join()
+        print(clientConnected_Socket)
+        print(threading.active_count())
     server.close()
 
 #function thread
@@ -75,11 +87,13 @@ def clientthread(clientSock,clientAddress):
         clientConnected_SessionKey.append(en_digest)
         clientConnected_Socket.append(clientSock)
 
-        clientSock.send(welcomeMsg.encode("UTF-8"))
-        while True:      
-            #message from client
-            recveMsg(en_digest,clientSock)
-        clientSock.close()
+        # print(clientConnected_Socket)
+        # gửi tin nhắn chào mừng
+        # clientSock.send(welcomeMsg.encode("UTF-8"))
+        while True:           
+            threadSend = threading.Thread(target=sendeMSg,args=(en_digest,clientSock,clientConnected_SessionKey,clientConnected_Socket,))
+            threadSend.start()
+            threadSend.join()
     else:
         print("Public key not match")
 
@@ -89,13 +103,88 @@ def recveMsg(key,socket):
     nonce = base64.b64decode(b64['nonce'])
     ct = base64.b64decode(b64['ciphertext'])
     # print(eMsg)
-    key = key[:16].encode()
+    key = key[:16].encode()   
     # print(key)
     aesDecrypt = AES.new(key,AES.MODE_CTR,nonce=nonce)
     # print(aesDecrypt)
     dMsg = aesDecrypt.decrypt(ct).decode()
     print("New mess from client ",socket ," : " , dMsg)
+    return dMsg
 
+
+def sendeMSg(recvKey,recvSocket,keyArr,socketArr):
+    #giải mã tin nhắn
+    dMsg = recveMsg(recvKey,recvSocket)
+    #tách chuỗi để xét trường hợp
+    MsgArr = dMsg.split()
+    # đăng ký
+    if (MsgArr[0] == "signup"):
+        sql = "INSERT INTO customers (name, password) VALUES (%s, %s)"
+        val = (MsgArr[1], MsgArr[2])
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(mycursor.rowcount, "record inserted.")
+        if (mycursor.rowcount):
+            SuccessMsg = ("Create new user successfully")
+            sendMsg = SuccessMsg.encode()
+            key = recvKey[:16].encode()
+            # print(key)
+            aesEncrypt = AES.new(key,AES.MODE_CTR)
+            # print(aesEncrypt)
+            ct_bytes = aesEncrypt.encrypt(sendMsg)
+            nonce = base64.b64encode(aesEncrypt.nonce).decode('utf-8')
+            ct = base64.b64encode(ct_bytes).decode('utf-8')
+            eMsg = json.dumps({'nonce':nonce, 'ciphertext':ct}).encode()
+            # print(eMsg)    
+            recvSocket.send(eMsg)
+    if (MsgArr[0] == "login"):
+        sql = "SELECT * FROM customers WHERE name = %s AND password = %s"
+        val = (MsgArr[1], MsgArr[2])
+        mycursor.execute(sql, val)
+        myresult = mycursor.fetchall()
+        if (myresult):
+            SuccessMsg = ("Login successfully")
+            sendMsg = SuccessMsg.encode()
+            key = recvKey[:16].encode()
+            # print(key)
+            aesEncrypt = AES.new(key,AES.MODE_CTR)
+            # print(aesEncrypt)
+            ct_bytes = aesEncrypt.encrypt(sendMsg)
+            nonce = base64.b64encode(aesEncrypt.nonce).decode('utf-8')
+            ct = base64.b64encode(ct_bytes).decode('utf-8')
+            eMsg = json.dumps({'nonce':nonce, 'ciphertext':ct}).encode()
+            # print(eMsg)    
+            recvSocket.send(eMsg)
+            #gửi danh sách người dùng sang cho client
+            sql = "SELECT name FROM customers"
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+            
+            sendMsg= listName.join(myresult).encode
+            key = recvKey[:16].encode()
+            aesEncrypt = AES.new(key,AES.MODE_CTR)
+            ct_bytes = aesEncrypt.encrypt(sendMsg)
+            nonce = base64.b64encode(aesEncrypt.nonce).decode('utf-8')
+            ct = base64.b64encode(ct_bytes).decode('utf-8')
+            eMsg = json.dumps({'nonce':nonce, 'ciphertext':ct}).encode()   
+            recvSocket.send(eMsg)
+    else:
+        sendMsg = dMsg.encode()
+        for i,s in enumerate(socketArr) :
+            if (s != recvSocket):
+                key = keyArr[i][:16].encode()
+                # print(key)
+                aesEncrypt = AES.new(key,AES.MODE_CTR)
+                # print(aesEncrypt)
+                ct_bytes = aesEncrypt.encrypt(sendMsg)
+                nonce = base64.b64encode(aesEncrypt.nonce).decode('utf-8')
+                ct = base64.b64encode(ct_bytes).decode('utf-8')
+                eMsg = json.dumps({'nonce':nonce, 'ciphertext':ct}).encode()
+                # print(eMsg)    
+                socketArr[i].send(eMsg)
+                print("send to Socket ",socketArr[i])
 
 if __name__ == "__main__":
+    mydb = mysql.connector.connect(host='localhost',database='anm',user='anm',password='123abnkakashi',port='1998')
+    mycursor = mydb.cursor()
     main() 
